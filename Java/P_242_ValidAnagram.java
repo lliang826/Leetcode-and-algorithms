ebadc6509058f96daf896108e9e8ff92ebdacd3d
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-10-31 21:03:46 -0700

    Added comment explanations

--- Java/P_242_ValidAnagram.java
+++ Java/P_242_ValidAnagram.java
@@ -1,6 +1,34 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * HashMap solution below: this solution requires us to iterate through each character in both strings to check if
+ * they are anagrams of each other. When we iterate through the first string, we can use a hashmap (fast lookup, O(1) time)
+ * to store the key: value pair as char: # of times the char appears in the string. If the char already exists in the 
+ * map, increment the counter. Otherwise, add it to the map. 
+ * Then, when we iterate through the second string, every time we visit a character, we decrement the value from the
+ * map instead of incrementing it. If the character doesn't exist in the map, we can return false because that means
+ * it doesn't exist in the first string. We can also add a small if statement to check the 2 string lengths (edge case).
+ * Finally, we have a third for loop to iterate through the values in the hashmap. If there are non-zero values, we return
+ * false. A positive value means an extra character in string s and a negative value means an extra character in string t.
+ * If the third loop terminates, we can conclude that the two strings are anagrams of each other.
+ * Time complexity: O(n), where n is the number of characters in the strings
+ * Space complexity: O(n), storing all chars into the map
+ * 
+ * Sorting solution: sort both strings in either ascending or descending order. Use a third for loop to iterate through
+ * the indices of both strings at the same time; if any characters don't match, return false.
+ * Time complexity: O(n log n), constrained by the sorting
+ * Space complexity: O(1) if sorting in place with heapsort (can't be merge sort because that creates arrays)
+ * 
+ * Array solution: similar to the hasmap solution, but if the question specifies only lowercase or only uppercase letters,
+ * we can use an array to store the counters instead of a map. The index of the array represents the ASCII value of the 
+ * letter (if the letter is 'c', then 'c' - 'a' = index 2). A second for loop is required to check for non-zero values
+ * in the array. This solution is slightly faster than the hashmap solution because arrays are a bit faster and more
+ * memory-efficient.
+ * Time complexity: O(n), iterating through all chars in the strings
+ * Space: O(n), storing all chars into an array
+ */
+
 public class P_242_ValidAnagram {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
