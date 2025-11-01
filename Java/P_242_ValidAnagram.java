43084f3981ab706be977c4820f06c77dc3bac118
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-10-31 21:03:32 -0700

    Added improved method for 242 Valid Anagram

--- Java/P_242_ValidAnagram.java
+++ Java/P_242_ValidAnagram.java
@@ -38,6 +38,43 @@ public class P_242_ValidAnagram {
         return true;
     }
 
+    public boolean isAnagramImproved(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                int value = map.get(c);
+                map.replace(c, value + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (map.containsKey(c)) {
+                int value = map.get(c);
+                if (value == 1) {               // Improvement here: instead of leaving counts of 0 in the map,
+                    map.remove(c);              // remove from from the hashmap so we can avoid the third for
+                }                               // loop and use a simple isEmpty() check instead
+                map.replace(c, value - 1);
+            } else {
+                return false;
+            }
+        }
+
+        if (!map.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         P_242_ValidAnagram solver = new P_242_ValidAnagram();
 
@@ -61,6 +98,22 @@ public class P_242_ValidAnagram {
             String t = tests[i][1];
             boolean expected = Boolean.parseBoolean(tests[i][2]);
             boolean actual = solver.isAnagram(s, t);
+            boolean ok = (expected == actual);
+            if (ok)
+                pass++;
+            System.out.printf("Test %d: s=\"%s\", t=\"%s\" => expected=%b, actual=%b => %s\n",
+                    i + 1, s, t, expected, actual, (ok ? "PASS" : "FAIL"));
+        }
+
+        System.out.printf("\nSummary: %d/%d tests passed.\n", pass, tests.length);
+
+        System.out.println("Running tests for P_242_ValidAnagram.isAnagramImproved\n");
+        pass = 0;
+        for (int i = 0; i < tests.length; i++) {
+            String s = tests[i][0];
+            String t = tests[i][1];
+            boolean expected = Boolean.parseBoolean(tests[i][2]);
+            boolean actual = solver.isAnagramImproved(s, t);
             boolean ok = (expected == actual);
             if (ok)
                 pass++;
