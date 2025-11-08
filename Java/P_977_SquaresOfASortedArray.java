933e82cf3f1f6cf1ef8378787a6a7392a1802ef6
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-11-08 11:14:59 -0800

    Added Java solution for 977 Squares of a Sorted Array

---
+++ Java/P_977_SquaresOfASortedArray.java
@@ -0,0 +1,59 @@
+public class P_977_SquaresOfASortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] output = new int[nums.length];
+               
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int leftInt = nums[left] * nums[left];
+            int rightInt = nums[right] * nums[right];
+            
+            if (leftInt > rightInt) {
+                output[i] = leftInt;
+                left++;
+            } else {
+                output[i] = rightInt;
+                right--;
+            }
+        }
+        
+        return output;
+    }
+
+    public static void main(String[] args) {
+        P_977_SquaresOfASortedArray solver = new P_977_SquaresOfASortedArray();
+
+        // Test cases: {input, expected}
+        Object[][] tests = new Object[][] {
+                { new int[]{-4, -1, 0, 3, 10}, new int[]{0, 1, 9, 16, 100} },
+                { new int[]{-7, -3, 2, 3, 11}, new int[]{4, 9, 9, 49, 121} },
+                { new int[]{-5, -3, -2, -1}, new int[]{1, 4, 9, 25} },
+                { new int[]{1, 2, 3, 4, 5}, new int[]{1, 4, 9, 16, 25} },
+                { new int[]{0}, new int[]{0} },
+                { new int[]{-1}, new int[]{1} },
+                { new int[]{2}, new int[]{4} },
+                { new int[]{-3, -2, -1, 0, 1, 2, 3}, new int[]{0, 1, 1, 4, 4, 9, 9} },
+                { new int[]{-10, -5, 0, 5, 10}, new int[]{0, 25, 25, 100, 100} },
+                { new int[]{-2, 0, 1}, new int[]{0, 1, 4} },
+                { new int[]{-1, 0, 1, 2}, new int[]{0, 1, 1, 4} },
+                { new int[]{-6, -4, -2, 1, 3}, new int[]{1, 4, 9, 16, 36} }
+        };
+
+        System.out.println("Running tests for P_977_SquaresOfASortedArray.sortedSquares\n");
+        int pass = 0;
+        for (int i = 0; i < tests.length; i++) {
+            int[] input = (int[]) tests[i][0];
+            int[] expected = (int[]) tests[i][1];
+            int[] actual = solver.sortedSquares(input.clone()); // Clone to avoid modifying original
+            
+            boolean ok = java.util.Arrays.equals(expected, actual);
+            if (ok)
+                pass++;
+            System.out.printf("Test %d: input=%s => expected=%s, actual=%s => %s\n",
+                    i + 1, java.util.Arrays.toString(input), java.util.Arrays.toString(expected), 
+                    java.util.Arrays.toString(actual), (ok ? "PASS" : "FAIL"));
+        }
+
+        System.out.printf("\nSummary: %d/%d tests passed.\n", pass, tests.length);
+    }
+}
