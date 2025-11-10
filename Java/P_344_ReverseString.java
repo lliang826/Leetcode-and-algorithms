44e17882323f2017fa3eb3427932d6f012fd6c43
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-11-09 22:51:11 -0800

    Added another solution for 344 Reverse String

--- Java/P_344_ReverseString.java
+++ Java/P_344_ReverseString.java
@@ -1,3 +1,8 @@
+/*
+ * Very simple algorithm involving 2 pointers. The left pointer is at the first index in the array,
+ * whereas the right pointer is at the last index. In each iteration, we swap the values at both
+ * indices. 
+ */
 public class P_344_ReverseString {
     public void reverseString(char[] s) {
         int left = 0;
@@ -10,6 +15,17 @@ public class P_344_ReverseString {
             
             left++;
             right--;
+        }
+    }
+
+    public void v2(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            int left = i;
+            int right = s.length - 1 - i;
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
         }
     }
 
@@ -54,5 +70,32 @@ public class P_344_ReverseString {
         }
 
         System.out.printf("\nSummary: %d/%d tests passed.\n", pass, tests.length);
+
+        // Test the v2() method
+        System.out.println("\n" + "=".repeat(50));
+        System.out.println("Running tests for P_344_ReverseString.v2\n");
+        int passV2 = 0;
+        for (int i = 0; i < tests.length; i++) {
+            String input = tests[i][0];
+            String expected = tests[i][1];
+            
+            // Convert input to char array for the method
+            char[] inputArray = input.toCharArray();
+            solver.v2(inputArray);
+            String actual = new String(inputArray);
+            
+            boolean ok = expected.equals(actual);
+            if (ok)
+                passV2++;
+            System.out.printf("Test %d: input=\"%s\" => expected=\"%s\", actual=\"%s\" => %s\n",
+                    i + 1, input, expected, actual, (ok ? "PASS" : "FAIL"));
+        }
+
+        System.out.printf("\nSummary: %d/%d tests passed.\n", passV2, tests.length);
+
+        System.out.println("\n" + "=".repeat(50));
+        System.out.printf("Overall Summary:\n");
+        System.out.printf("reverseString: %d/%d tests passed\n", pass, tests.length);
+        System.out.printf("v2: %d/%d tests passed\n", passV2, tests.length);
     }
 }
