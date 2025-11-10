ec106e5ceabe91dceb94ad6ab0b7ee62cbf5e049
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-11-09 23:18:15 -0800

    Added alternate solution and explanation for 977

--- Java/P_977_SquaresOfASortedArray.java
+++ Java/P_977_SquaresOfASortedArray.java
@@ -1,3 +1,15 @@
+/*
+ * Requires the two pointer technique. Since the input array holds both negative and positive integers but it is sorted,
+ * we know that the largest squares will be at either the left and right ends of the array, or both.
+ * [-2, -1, 0, 1, 2] => [4, 1, 0, 1, 4]
+ * [-1, 0, 1, 2, 3] => [1, 0, 1, 4, 9]
+ * [-3, -2, -1, 0, 1] => [9, 4, 1, 0, 1]
+ * 
+ * We can therefore initialize a left pointer at the start of the input array and a right pointer at the end. In each
+ * iteration, we compare the squares at each pointer. The larger number is stored in the output array and that pointer
+ * moves towards the center of the array.
+ * Storing the number in the output array requires an additional variable to track the index.
+ */
 public class P_977_SquaresOfASortedArray {
     public int[] sortedSquares(int[] nums) {
         int left = 0;
@@ -15,6 +27,29 @@ public class P_977_SquaresOfASortedArray {
                 output[i] = rightInt;
                 right--;
             }
+        }
+        
+        return output;
+    }
+
+    public int[] v2(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] output = new int[nums.length];
+        int index = nums.length - 1;
+               
+        while (left <= right) {
+            int leftInt = (int) Math.pow(nums[left], 2);
+            int rightInt = (int) Math.pow(nums[right], 2);
+
+            if (leftInt >= rightInt) {
+                output[index] = leftInt;
+                left++;
+            } else {
+                output[index] = rightInt;
+                right--;
+            }
+            index--;
         }
         
         return output;
@@ -55,5 +90,28 @@ public class P_977_SquaresOfASortedArray {
         }
 
         System.out.printf("\nSummary: %d/%d tests passed.\n", pass, tests.length);
+        
+        System.out.println("\n" + "=".repeat(50));
+        System.out.println("Running tests for P_977_SquaresOfASortedArray.v2\n");
+        int passV2 = 0;
+        for (int i = 0; i < tests.length; i++) {
+            int[] input = (int[]) tests[i][0];
+            int[] expected = (int[]) tests[i][1];
+            int[] actual = solver.v2(input.clone()); // Clone to avoid modifying original
+            
+            boolean ok = java.util.Arrays.equals(expected, actual);
+            if (ok)
+                passV2++;
+            System.out.printf("Test %d: input=%s => expected=%s, actual=%s => %s\n",
+                    i + 1, java.util.Arrays.toString(input), java.util.Arrays.toString(expected), 
+                    java.util.Arrays.toString(actual), (ok ? "PASS" : "FAIL"));
+        }
+
+        System.out.printf("\nSummary: %d/%d tests passed.\n", passV2, tests.length);
+
+        System.out.println("\n" + "=".repeat(50));
+        System.out.printf("Overall Summary:\n");
+        System.out.printf("sortedSquares: %d/%d tests passed\n", pass, tests.length);
+        System.out.printf("v2: %d/%d tests passed\n", passV2, tests.length);
     }
 }
