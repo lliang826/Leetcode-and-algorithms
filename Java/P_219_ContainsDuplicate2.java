0a690fd235531cdb5f88199f48c5b06d2d11576b
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-11-18 23:40:01 -0800

    Added v2

--- Java/P_219_ContainsDuplicate2.java
+++ Java/P_219_ContainsDuplicate2.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class P_219_ContainsDuplicate2 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
@@ -14,7 +16,26 @@ public class P_219_ContainsDuplicate2 {
         return false;
     }
 
+    public boolean v2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (set.contains(nums[right])) {
+                return true;
+            } else {
+                set.add(nums[right]);
+            }
+            
+            if (right >= k) {
+                set.remove(nums[left]);
+                left++;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
