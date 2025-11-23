6c731fede7b45eddca78cd34486bb28e754a7399
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-11-23 15:40:38 -0800

    Added comments

--- Java/P_1426_CountingElements.java
+++ Java/P_1426_CountingElements.java
@@ -1,6 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+Simple hashmap solution below. First, we initialize an empty hashmap and iterate through the input array to store all the 
+integers and their counts into the hashmap. Then, we run a second loop through all the keys of the hashmap: if key + 1
+also exists in the hashmap, add the count of key to the sum. 
+
+Time: O(2 * n) => O(n), we must iterate through all integers in the input array to create the hashmap
+Space: O(n), all integers from the input array may be added to the hashmap
+
+
+There is also a brute force solution, which requires a nested for loop. Space complexity is better at O(1), but time
+complexity is drastically worse at O(n^2).
+
+Finally, there is a sorting solution. After sorting the integers into ascending order, we can perform a second loop to
+check how many integers x there are, and we can also check if x + 1 exists.
+Time: O(n log n) because of sorting
+Space: O(1) if we can sort in place, O(n) if we duplicate the input array
+*/
 public class P_1426_CountingElements {
     public int countElements(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
