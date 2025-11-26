ae716088d8b965a31b5e94dee79597274bf71279
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-11-25 22:51:12 -0800

    Added java solution for 1248. Count Number of Nice Subarrays

---
+++ Java/P_1248_CountNumberOfNiceSubarrays.java
@@ -0,0 +1,107 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class P_1248_CountNumberOfNiceSubarrays {
+    public int numberOfSubarrays(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int oddCount = 0;
+        int niceCount = 0;
+
+        for (int num : nums) {
+            if (num % 2 == 1) {
+                oddCount++;
+            }
+
+            int prefixComplement = oddCount - k;
+            if (map.containsKey(prefixComplement)) {
+                niceCount += map.get(prefixComplement);
+            }
+
+            map.put(oddCount, map.getOrDefault(oddCount, 0) + 1);
+        }
+
+        return niceCount;
+    }
+
+    public int v2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int oddCount = 0;
+        int niceCount = 0;
+
+        for (int num : nums) {
+            oddCount += num % 2;
+            niceCount += map.getOrDefault(oddCount - k, 0);
+            map.put(oddCount, map.getOrDefault(oddCount, 0) + 1);
+        }
+
+        return niceCount;
+    }
+
+    public static void main(String[] args) {
+        P_1248_CountNumberOfNiceSubarrays solver = new P_1248_CountNumberOfNiceSubarrays();
+
+        Object[][] tests = new Object[][] {
+                { new int[]{1, 1, 2, 1, 1}, 3, 2 },
+                { new int[]{2, 4, 6}, 1, 0 },
+                { new int[]{2, 2, 2, 1, 2, 2, 1, 2, 2, 2}, 2, 16 },
+                { new int[]{1, 1, 1, 1, 1}, 1, 5 },
+                { new int[]{1, 1, 1, 1, 1}, 5, 1 },
+                { new int[]{2, 2, 2, 2}, 1, 0 },
+                { new int[]{1}, 1, 1 },
+                { new int[]{2}, 1, 0 },
+                { new int[]{1, 2, 3, 4, 5}, 2, 4 },
+                { new int[]{1, 1, 2, 2, 3}, 3, 1 },
+                { new int[]{2, 2, 1, 2, 1, 2, 1}, 3, 3 },
+                { new int[]{1, 3, 5}, 1, 3 },
+                { new int[]{1, 3, 5}, 2, 2 },
+                { new int[]{1, 3, 5}, 3, 1 },
+                { new int[]{2, 4, 1, 6, 8, 3, 10}, 2, 6 }
+        };
+
+        System.out.println("Running tests for P_1248_CountNumberOfNiceSubarrays.numberOfSubarrays\n");
+        int pass1 = 0;
+        for (int i = 0; i < tests.length; i++) {
+            int[] input = (int[]) tests[i][0];
+            int k = (int) tests[i][1];
+            int expected = (int) tests[i][2];
+            int actual = solver.numberOfSubarrays(input.clone(), k);
+            
+            boolean ok = expected == actual;
+            if (ok)
+                pass1++;
+            System.out.printf("Test %d: input=%s, k=%d => expected=%d, actual=%d => %s\n",
+                    i + 1, java.util.Arrays.toString(input), k, expected, actual, 
+                    (ok ? "PASS" : "FAIL"));
+        }
+
+        System.out.printf("\nSummary: %d/%d tests passed.\n", pass1, tests.length);
+
+        System.out.println("\n" + "=".repeat(50));
+        System.out.println("Running tests for P_1248_CountNumberOfNiceSubarrays.v2\n");
+        int pass2 = 0;
+        for (int i = 0; i < tests.length; i++) {
+            int[] input = (int[]) tests[i][0];
+            int k = (int) tests[i][1];
+            int expected = (int) tests[i][2];
+            int actual = solver.v2(input.clone(), k);
+            
+            boolean ok = expected == actual;
+            if (ok)
+                pass2++;
+            System.out.printf("Test %d: input=%s, k=%d => expected=%d, actual=%d => %s\n",
+                    i + 1, java.util.Arrays.toString(input), k, expected, actual, 
+                    (ok ? "PASS" : "FAIL"));
+        }
+
+        System.out.printf("\nSummary: %d/%d tests passed.\n", pass2, tests.length);
+        
+        System.out.println("\n" + "=".repeat(50));
+        System.out.printf("Overall Summary:\n");
+        System.out.printf("numberOfSubarrays: %d/%d tests passed\n", pass1, tests.length);
+        System.out.printf("v2: %d/%d tests passed\n", pass2, tests.length);
+    }
+}
