938350511afb82c3533344571e2b518fc2f087b0
Lawrence Liang <lawrence.liang@hotmail.ca>
2025-12-07 18:11:46 -0800

    Add test cases

--- Java/P_2352_EqualRowAndColumnPairs.java
+++ Java/P_2352_EqualRowAndColumnPairs.java
@@ -33,6 +33,33 @@ public class P_2352_EqualRowAndColumnPairs {
     }
 
     public static void main(String[] args) {
-        
+        P_2352_EqualRowAndColumnPairs solver = new P_2352_EqualRowAndColumnPairs();
+
+        Object[][] tests = new Object[][] {
+                { new int[][] { { 3, 2, 1 }, { 1, 7, 6 }, { 2, 7, 7 } }, 1 },
+                { new int[][] { { 3, 1, 2, 2 }, { 1, 4, 4, 5 }, { 2, 4, 2, 2 }, { 2, 4, 2, 2 } }, 3 },
+                { new int[][] { { 1 } }, 1 },
+                { new int[][] { { 1, 2 }, { 3, 4 } }, 0 },
+                { new int[][] { { 1, 2 }, { 2, 1 } }, 2 },
+                { new int[][] { { 11, 1 }, { 1, 11 } }, 2 },
+                { new int[][] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } }, 9 },
+                { new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, 0 },
+                { new int[][] { { 13, 13 }, { 13, 13 } }, 4 }
+        };
+
+        System.out.println("Running tests for P_2352_EqualRowAndColumnPairs.equalPairs\n");
+        int pass = 0;
+        for (int i = 0; i < tests.length; i++) {
+            int[][] input = (int[][]) tests[i][0];
+            int expected = (int) tests[i][1];
+            int actual = solver.equalPairs(input);
+
+            boolean ok = expected == actual;
+            if (ok)
+                pass++;
+            System.out.printf("Test %d: expected=%d, actual=%d => %s\n",
+                    i + 1, expected, actual, (ok ? "PASS" : "FAIL"));
+        }
+        System.out.printf("\nSummary: %d/%d tests passed.\n", pass, tests.length);
     }
 }
