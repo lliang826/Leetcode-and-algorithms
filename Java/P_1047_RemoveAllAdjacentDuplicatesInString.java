0dce897f7e906fc33161ff9db3381a8e137844e1
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-01-03 01:25:39 -0800

    Add test cases and analysis

--- Java/P_1047_RemoveAllAdjacentDuplicatesInString.java
+++ Java/P_1047_RemoveAllAdjacentDuplicatesInString.java
@@ -1,6 +1,14 @@
 import java.util.Stack;
 
 public class P_1047_RemoveAllAdjacentDuplicatesInString {
+    /*
+    Stack approach. We pop any adjacent duplicate characters and push other characters to the stack.
+    Finally, we have to use StringBuilder to return the remaining characters in the stack as a string.
+    
+    Time: O(n), where n is the number of characters in the input string
+    Space: O(n), in the worst case scenario, there are no adjacent duplicate characters, which means
+    that all characters are added to the stack
+    */
     public String removeDuplicates(String s) {
         Stack<Character> stack = new Stack<>();
 
@@ -18,5 +26,47 @@ public class P_1047_RemoveAllAdjacentDuplicatesInString {
             sb.append(stack.get(i));
         }
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        P_1047_RemoveAllAdjacentDuplicatesInString solver = new P_1047_RemoveAllAdjacentDuplicatesInString();
+
+        Object[][] tests = new Object[][] {
+                { "abbaca", "ca" },
+                { "azxxzy", "ay" },
+                { "a", "a" },
+                { "aa", "" },
+                { "aabbcc", "" },
+                { "abccba", "" },
+                { "abcd", "abcd" },
+                { "aabbccdd", "" },
+                { "aabbccdde", "e" },
+                { "aaaaa", "a" },
+                { "aaaa", "" },
+                { "abba", "" },
+                { "abbbba", "" },
+                { "mississippi", "m" },
+                { "aabbaabb", "" },
+                { "xyzxyz", "xyzxyz" }
+        };
+
+        System.out.println("Running tests for P_1047_RemoveAllAdjacentDuplicatesInString.removeDuplicates\n");
+        int pass1 = 0;
+        for (int i = 0; i < tests.length; i++) {
+            String input = (String) tests[i][0];
+            String expected = (String) tests[i][1];
+            String actual = solver.removeDuplicates(input);
+
+            boolean ok = expected.equals(actual);
+            if (ok)
+                pass1++;
+            System.out.printf("Test %d: input=\"%s\" => expected=\"%s\", actual=\"%s\" => %s\n",
+                    i + 1, input, expected, actual, (ok ? "PASS" : "FAIL"));
+        }
+        System.out.printf("\nSummary: %d/%d tests passed.\n", pass1, tests.length);
+
+        System.out.println("\n" + "=".repeat(50));
+        System.out.printf("Overall Summary:\n");
+        System.out.printf("removeDuplicates: %d/%d tests passed\n", pass1, tests.length);
     }
 }
