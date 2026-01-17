7a295d0993737f3f0af49d44c553c3b58b1005db
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-01-16 20:12:14 -0800

    Add another solution for 167

--- Java/P_167_TwoSum2InputArrayIsSorted.java
+++ Java/P_167_TwoSum2InputArrayIsSorted.java
@@ -17,6 +17,24 @@ public class P_167_TwoSum2InputArrayIsSorted {
         return new int[] { left, right };
     }
     
+    public int[] v2(int[] numbers, int target) {
+        int left = 1;
+        int right = numbers.length;
+
+        while (left < right) {
+            int sum = numbers[left - 1] + numbers[right - 1];
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return new int[] { left, right };
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+    
     public static void main(String[] args) {
         
     }
