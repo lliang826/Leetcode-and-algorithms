0083850baa59c96a876c771cabc20ce68c6a272a
lliang826 <xlawrence.liangx@gmail.com>
2026-01-18 17:36:54 -0800

    Add improved sliding window monotonic queue solution

--- Java/P_1438_LongestContinuousSubarray.java
+++ Java/P_1438_LongestContinuousSubarray.java
@@ -1,5 +1,8 @@
-public class P_1438_LongestContinuousSubbarray {
-    public int longestSubarray(int[] nums, int limit) {
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class P_1438_LongestContinuousSubarray {
+    public int longestSubarrayV1(int[] nums, int limit) {
         Deque<Integer> inc = new ArrayDeque<>();
         Deque<Integer> dec = new ArrayDeque<>();
         int max = 0;
@@ -31,7 +34,35 @@ public class P_1438_LongestContinuousSubbarray {
         return max;
     }
 
-    public static void main(String[] args) {
-        
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> inc = new ArrayDeque<>();
+        Deque<Integer> dec = new ArrayDeque<>();
+        int max = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while (!inc.isEmpty() && inc.peekLast() > nums[right]) {
+                inc.removeLast();
+            }
+            while (!dec.isEmpty() && dec.peekLast() < nums[right]) {
+                dec.removeLast();
+            }
+            inc.addLast(nums[right]);
+            dec.addLast(nums[right]);
+
+            while (dec.peekFirst() - inc.peekFirst() > limit) {
+                if (!inc.isEmpty() && inc.peekFirst() == nums[left]) {
+                    inc.removeFirst();
+                }
+                if (!dec.isEmpty() && dec.peekFirst() == nums[left]) {
+                    dec.removeFirst();
+                }
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
     }
 }
