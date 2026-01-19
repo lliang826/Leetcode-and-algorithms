6699d7f0087e3002589ba30a9e0df57ef67bbf3d
lliang826 <xlawrence.liangx@gmail.com>
2026-01-18 17:26:22 -0800

    Initial Java solution for 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ Java/P_1438_LongestContinuousSubarray.java
@@ -0,0 +1,37 @@
+public class P_1438_LongestContinuousSubbarray {
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> inc = new ArrayDeque<>();
+        Deque<Integer> dec = new ArrayDeque<>();
+        int max = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while (!inc.isEmpty() && inc.peekLast() > nums[right]) {
+                inc.removeLast();
+            }
+            while (!dec.isEmpty() && dec.peekLast() < nums[right]) {
+                dec.removeLast();
+            }
+            inc.addLast(nums[right]);
+            dec.addLast(nums[right]);
+
+            if (Math.abs(inc.peekFirst() - dec.peekFirst()) <= limit) {
+                max = Math.max(max, right - left + 1);
+            } else {
+                if (!inc.isEmpty() && inc.peekFirst() == nums[left]) {
+                    inc.removeFirst();
+                }
+                if (!dec.isEmpty() && dec.peekFirst() == nums[left]) {
+                    dec.removeFirst();
+                }
+                left++;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
