506f077844a043613f52e7f42fec74ab97b382af
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-01-23 00:10:08 -0800

    Add improved solutions using stack with hashmap

--- Java/P_496_NextGreaterElement1.java
+++ Java/P_496_NextGreaterElement1.java
@@ -1,3 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 public class P_496_NextGreaterElement1 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
@@ -16,6 +21,49 @@ public class P_496_NextGreaterElement1 {
             if (ans[i] == 0) {
                 ans[i] = -1;
             }
+        }
+
+        return ans;
+    }
+
+    public int[] v2(int[] nums1, int[] nums2) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums2) {
+            while (!deque.isEmpty() && deque.peekLast() < num) {
+                map.put(deque.peekLast(), num);
+                deque.removeLast();
+            }
+            deque.addLast(num);
+        }
+
+        while (!deque.isEmpty()) {
+            map.put(deque.removeLast(), -1);
+        }
+
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+
+    public int[] v3(int[] nums1, int[] nums2) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums2) {
+            while (!deque.isEmpty() && deque.peekLast() < num) {
+                map.put(deque.removeLast(), num);
+            }
+            deque.addLast(num);
+        }
+
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.getOrDefault(nums1[i], -1);
         }
 
         return ans;
