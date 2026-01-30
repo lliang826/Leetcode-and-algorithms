31d5fb6f8e1d4f551284759bb108e485575c395c
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-01-29 23:58:03 -0800

    Add Java solution for 112. Path Sum

---
+++ Java/P_112_PathSum.java
@@ -0,0 +1,17 @@
+import utils.TreeNode;
+
+public class P_112_PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return targetSum - root.val == 0;
+        }
+
+        boolean left = hasPathSum(root.left, targetSum - root.val);
+        boolean right = hasPathSum(root.right, targetSum - root.val);
+        return left || right;
+    }
+}
