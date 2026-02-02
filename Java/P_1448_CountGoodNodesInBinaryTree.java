b6634b26106e0bda704869f8c7627829fde25743
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-02-01 17:00:23 -0800

    Add Java solution for 1448. Count Good Nodes in Binary Tree

---
+++ Java/P_1448_CountGoodNodesInBinaryTree.java
@@ -0,0 +1,23 @@
+import utils.TreeNode;
+
+public class P_1448_CountGoodNodesInBinaryTree {
+    public int goodNodes(TreeNode root) {
+        return dfs(root, Integer.MIN_VALUE);
+    }
+
+    public int dfs(TreeNode root, int max) {
+        if (root == null) {
+            return 0;
+        }
+
+        int curr = 0;
+        if (root.val >= max) {
+            curr = 1;
+        }
+
+        max = Math.max(root.val, max);
+        int left = dfs(root.left, max);
+        int right = dfs(root.right, max);
+        return left + right + curr;
+    }
+}
