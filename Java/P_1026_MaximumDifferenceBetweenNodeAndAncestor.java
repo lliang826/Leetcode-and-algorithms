324dee7b932d9cdea036c8952c100b3b743a4fb5
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-02-06 23:41:43 -0800

    Add optimized/cleaner solutions

--- Java/P_1026_MaximumDifferenceBetweenNodeAndAncestor.java
+++ Java/P_1026_MaximumDifferenceBetweenNodeAndAncestor.java
@@ -22,4 +22,46 @@ public class P_1026_MaximumDifferenceBetweenNodeAndAncestor {
             return Math.max(diff, maxChildDiff);
         }
     }
+
+    class Solution2 {
+        public int maxAncestorDiff(TreeNode root) {
+            return dfs(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        public int dfs(TreeNode root, int min, int max) {
+            if (root == null) {
+                return 0;
+            }
+
+            min = Math.min(min, root.val);
+            max = Math.max(max, root.val);
+            int diff = max - min;
+
+            int left = dfs(root.left, min, max);
+            int right = dfs(root.right, min, max);
+
+            int maxChildDiff = Math.max(left, right);
+            return Math.max(diff, maxChildDiff);
+        }
+    }
+
+    class Solution3 {
+        public int maxAncestorDiff(TreeNode root) {
+            return dfs(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        public int dfs(TreeNode root, int min, int max) {
+            if (root == null) {
+                return max - min;
+            }
+
+            min = Math.min(min, root.val);
+            max = Math.max(max, root.val);
+
+            int left = dfs(root.left, min, max);
+            int right = dfs(root.right, min, max);
+
+            return Math.max(left, right);
+        }
+    }
 }
