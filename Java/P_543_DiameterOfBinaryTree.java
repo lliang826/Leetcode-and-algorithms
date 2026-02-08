146eca2e0977d67e9556a771f62dc46339814adc
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-02-07 22:44:57 -0800

    Add Java solution for 543. Diameter of Binary Tree

---
+++ Java/P_543_DiameterOfBinaryTree.java
@@ -0,0 +1,25 @@
+import data_structures.TreeNode;
+
+public class P_543_DiameterOfBinaryTree {
+    class Solution {
+        int diameter = 0;
+
+        public int diameterOfBinaryTree(TreeNode root) {
+            dfs(root);
+            return diameter;
+        }
+
+        private int dfs(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int left = dfs(root.left);
+            int right = dfs(root.right);
+
+            diameter = Math.max(diameter, left + right);
+
+            return 1 + Math.max(left, right);
+        }
+    }
+}
