820e397b89eee84c7c52bfab4889ec36440f4cbf
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-02-13 22:22:51 -0800

    Add more concise Java solution for 236

--- Java/P_236_LowestCommonAncestorOfABinaryTree.java
+++ Java/P_236_LowestCommonAncestorOfABinaryTree.java
@@ -7,7 +7,7 @@ public class P_236_LowestCommonAncestorOfABinaryTree {
             return null;
         }
 
-        // Scenario 3 (example 3)
+        // Scenario 3
         if (root == p || root == q) {
             return root;
         }
@@ -15,16 +15,34 @@ public class P_236_LowestCommonAncestorOfABinaryTree {
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        // Scenario 1 (example 1)
+        // Scenario 1
         if (left != null && right != null) {
             return root;
         }
 
-        // Scenario 2 (example 2)
+        // Scenario 2
         if (left != null && right == null) {
             return left;
         }
 
         return right;
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        // Base case + scenario 3
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+
+        // Scenario 1
+        if (left != null && right != null) {
+            return root;
+        }
+
+        // Scenario 2
+        return left != null ? left : right;
     }
 }
