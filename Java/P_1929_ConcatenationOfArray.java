9c2a3933c9bacdd08c1498a69ecabd2508bbac82
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-03-01 23:18:24 -0800

    Add Java solution for 1929. Concatenation of Array

---
+++ Java/P_1929_ConcatenationOfArray.java
@@ -0,0 +1,15 @@
+public class P_1929_ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            res[i + nums.length] = nums[i];
+        }
+
+        return res;
+    }
+}
