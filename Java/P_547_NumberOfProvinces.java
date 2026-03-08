8fdc33cca69ae645901ef8d853f2eada640c5724
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-03-08 16:31:41 -0700

    Add more solutions for 547 Java

--- Java/P_547_NumberOfProvinces.java
+++ Java/P_547_NumberOfProvinces.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -88,6 +89,78 @@ public class P_547_NumberOfProvinces {
                 if (!seen.contains(neighbor)) {
                     seen.add(neighbor);
                     dfs(neighbor);
+                }
+            }
+        }
+    }
+
+    class Solution3 {
+        private Set<Integer> seen = new HashSet<>();
+        private HashMap<Integer, List<Integer>> graph = new HashMap<>();
+
+        public int findCircleNum(int[][] isConnected) {
+            this.buildGraph(isConnected);
+
+            int res = 0;
+            for (int i = 0; i < graph.size(); i++) {
+                if (!seen.contains(i)) {
+                    res++;
+                    dfs(i);
+                }
+            }
+
+            return res;
+        }
+
+        private void dfs(int node) {
+            seen.add(node);
+            for (int neighbor : graph.get(node)) {
+                if (!seen.contains(neighbor)) {
+                    dfs(neighbor);
+                }
+            }
+        }
+
+        private void buildGraph(int[][] isConnected) {
+            int n = isConnected.length;
+
+            for (int i = 0; i < n; i++) {
+                graph.put(i, new ArrayList<>());
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (isConnected[i][j] == 1) {
+                        graph.get(i).add(j);
+                        graph.get(j).add(i);
+                    }
+                }
+            }
+        }
+    }
+
+    class Solution4 {
+        private Set<Integer> seen = new HashSet<>();
+
+        public int findCircleNum(int[][] isConnected) {
+            int n = isConnected.length;
+            int res = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (!seen.contains(i)) {
+                    res += 1;
+                    dfs(isConnected, i);
+                }
+            }
+
+            return res;
+        }
+
+        private void dfs(int[][] isConnected, int node) {
+            seen.add(node);
+            for (int j = 0; j < isConnected.length; j++) {
+                if (isConnected[node][j] == 1 && !seen.contains(j)) {
+                    dfs(isConnected, j);
                 }
             }
         }
