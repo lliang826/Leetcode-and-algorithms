311a9b1d059fbf578879e8b8ce113e7914277354
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-03-23 22:49:01 -0700

    Add improved solution for 1971 Java

--- Java/P_1971_FindIfPathExistsInGraph.java
+++ Java/P_1971_FindIfPathExistsInGraph.java
@@ -36,4 +36,40 @@ public class P_1971_FindIfPathExistsInGraph {
             }
         }
     }
+
+    class Solution2 {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Set<Integer> seen = new HashSet<>();
+
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+            this.buildGraph(n, edges);
+            return this.dfs(source, destination);
+        }
+
+        private boolean dfs(int i, int destination) {
+            seen.add(i);
+            if (i == destination) {
+                return true;
+            }
+            for (int n : graph.get(i)) {
+                if (!seen.contains(n)) {
+                    if (dfs(n, destination)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void buildGraph(int n, int[][] edges) {
+            for (int i = 0; i < n; i++) {
+                graph.put(i, new ArrayList<>());
+            }
+
+            for (int[] edge : edges) {
+                graph.get(edge[0]).add(edge[1]);
+                graph.get(edge[1]).add(edge[0]);
+            }
+        }
+    }
 }
