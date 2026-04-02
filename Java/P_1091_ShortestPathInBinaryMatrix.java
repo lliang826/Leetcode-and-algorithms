7fe3140f9e72039fa50f3418f84578a23961575a
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-04-01 19:18:23 -0700

    Add analysis for 1091 Java

--- Java/P_1091_ShortestPathInBinaryMatrix.java
+++ Java/P_1091_ShortestPathInBinaryMatrix.java
@@ -2,6 +2,25 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class P_1091_ShortestPathInBinaryMatrix {
+    /*
+    BFS approach with 2D boolean array to track visited nodes. 
+    Since the problem asks for the shortest path, we use BFS since it visits nodes based on their distance to the
+    root. We also create a 2D array for the 8 directions (4 edges + 4 corners) so the code is cleaner.
+    We begin by adding the top left corner (0, 0) to the queue. BFS in graphs is basically the same as BFS in trees,
+    so we can use the same placeholder algorithm. 
+    Whenever we add a node to the queue, we mark it as visited. We can't do the reverse, where we mark a node as 
+    visited when it is dequeued, because if a node has multiple neighbors, it can potentially be enqueued multiple 
+    times.
+    We increment the pathLength counter every time we visit a new level, and we return this value if we reach the
+    bottom right corner (n - 1, n - 1). If we don't reach the bottom right corner after visiting all possible
+    neighbors, we return -1.
+    For each node, its neighbors are the 8 directions mentioned earlier; we also need a helper method to validate
+    if the neighboring node is within the n x n matrix.
+    
+    Time: O(n^2) because the matrix dimensions are n x n
+      - in the worst case scenario, we visit all nodes in the graph if they're all 0s
+    Space: O(n^2) because of the 2D boolean array
+    */
     class Solution {
         public int shortestPathBinaryMatrix(int[][] grid) {
             boolean[][] visited = new boolean[grid.length][grid.length];
