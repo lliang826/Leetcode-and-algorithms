44c1066add60e6d29945ba2f72a5fc97aeb353aa
lliang826 <xlawrence.liangx@gmail.com>
2026-04-08 00:38:41 -0700

    Initial Java solution for 1129. Shortest Path with Alternating Colors

---
+++ Java/P_1129_ShortestPathWithAlternatingColors.java
@@ -0,0 +1,79 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class P_1129_ShortestPathWithAlternatingColors {
+    class Solution {
+        Map<Integer, List<Integer>> redMap = new HashMap<>();
+        Map<Integer, List<Integer>> blueMap = new HashMap<>();
+
+        public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+            this.buildGraph(n, redEdges, blueEdges);
+
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = -1;
+            }
+            Queue<int[]> queue = new ArrayDeque<>();
+            boolean[][] visited = new boolean[n][2];
+            int level = 0;
+
+            queue.offer(new int[] { 0, 0 }); // 0 is red
+            visited[0][0] = true;
+            res[0] = 0;
+
+            queue.offer(new int[] { 0, 1 }); // 1 is blue
+            visited[0][1] = true;
+            res[0] = 0;
+
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+
+                for (int i = 0; i < levelSize; i++) {
+                    int[] node = queue.poll();
+                    if (res[node[0]] == -1) {
+                        res[node[0]] = level;
+                    }
+
+                    if (node[1] == 0) {
+                        for (int neighbor : blueMap.get(node[0])) {
+                            if (!visited[neighbor][1]) {
+                                queue.offer(new int[] { neighbor, 1 });
+                                visited[neighbor][1] = true;
+                            }
+                        }
+                    } else {
+                        for (int neighbor : redMap.get(node[0])) {
+                            if (!visited[neighbor][0]) {
+                                queue.offer(new int[] { neighbor, 0 });
+                                visited[neighbor][0] = true;
+                            }
+                        }
+                    }
+                }
+
+                level++;
+            }
+
+            return res;
+        }
+
+        private void buildGraph(int n, int[][] redEdges, int[][] blueEdges) {
+            for (int i = 0; i < n; i++) {
+                redMap.put(i, new ArrayList<>());
+                blueMap.put(i, new ArrayList<>());
+            }
+
+            for (int i = 0; i < redEdges.length; i++) {
+                redMap.get(redEdges[i][0]).add(redEdges[i][1]);
+            }
+
+            for (int i = 0; i < blueEdges.length; i++) {
+                blueMap.get(blueEdges[i][0]).add(blueEdges[i][1]);
+            }
+        }
+    }
+}
