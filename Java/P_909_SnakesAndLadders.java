164c823bb21387138ff7ac09c00d77f71182de10
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-04-26 18:25:48 -0700

    Add Java solution for 909. Snakes and Ladders

---
+++ Java/P_909_SnakesAndLadders.java
@@ -0,0 +1,59 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class P_909_SnakesAndLadders {
+    class Solution {
+        public int snakesAndLadders(int[][] board) {
+            int n = board.length;
+            Queue<Integer> queue = new ArrayDeque<>();
+            boolean[] seen = new boolean[n * n + 1];
+            int rolls = 0;
+
+            queue.offer(1);
+            seen[1] = true;
+
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+
+                for (int i = 0; i < levelSize; i++) {
+                    int node = queue.poll();
+
+                    if (node == n * n) {
+                        return rolls;
+                    }
+
+                    for (int next = node + 1; next <= node + 6 && next <= n * n; next++) {
+                        int[] c = boardSquareToMatrix(next, n);
+                        int dest = next;
+
+                        if (board[c[0]][c[1]] != -1) {
+                            dest = board[c[0]][c[1]];
+                        }
+
+                        if (!seen[dest]) {
+                            queue.offer(dest);
+                            seen[dest] = true;
+                        }
+                    }
+                }
+
+                rolls++;
+            }
+
+            return -1;
+        }
+
+        private int[] boardSquareToMatrix(int square, int n) {
+            int index = square - 1;
+            int rowFromBottom = index / n;
+            int row = n - 1 - rowFromBottom;
+            int col = index % n;
+
+            if (rowFromBottom % 2 == 1) {
+                col = n - 1 - col;
+            }
+
+            return new int[] { row, col };
+        }
+    }
+}
