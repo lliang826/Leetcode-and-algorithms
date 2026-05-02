7ab378996d81d76bbb5d37f2a7537cd9368ee6be
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-05-01 22:05:44 -0700

    Add substring approach for 752 Java

--- Java/P_752_OpenTheLock.java
+++ Java/P_752_OpenTheLock.java
@@ -68,6 +68,65 @@ public class P_752_OpenTheLock {
         }
     }
 
+    class Solution2 {
+        public int openLock(String[] deadends, String target) {
+            Queue<String> queue = new ArrayDeque<>();
+            Set<String> seen = new HashSet<>();
+            int count = 0;
+
+            for (String d : deadends) {
+                if (d.equals("0000")) {
+                    return -1;
+                }
+                seen.add(d);
+            }
+
+            queue.offer("0000");
+            seen.add("0000");
+
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+
+                for (int i = 0; i < levelSize; i++) {
+                    String node = queue.poll();
+
+                    if (node.equals(target)) {
+                        return count;
+                    }
+
+                    for (String turn : this.getNextTurns(node)) {
+                        if (!seen.contains(turn)) {
+                            queue.offer(turn);
+                            seen.add(turn);
+                        }
+                    }
+                }
+
+                count++;
+            }
+
+            return -1;
+        }
+
+        private String[] getNextTurns(String node) {
+            List<String> list = new ArrayList<>();
+
+            for (int i = 0; i < node.length(); i++) {
+                char c = node.charAt(i);
+
+                char rollUp = c == '9' ? '0' : (char) (c + 1);
+                String rollUpString = node.substring(0, i) + rollUp + node.substring(i + 1);
+                list.add(rollUpString);
+
+                char rollDown = c == '0' ? '9' : (char) (c - 1);
+                String rollDownString = node.substring(0, i) + rollDown + node.substring(i + 1);
+                list.add(rollDownString);
+            }
+
+            return list.toArray(new String[0]);
+        }
+    }
+
     /*
      * 0000
      * 
