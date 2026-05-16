c43ccccc7a16cda85d518cb1da4a5f58c63751fc
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-05-16 13:08:51 -0700

    Add BFS approach for 2101 Java

--- Java/P_2101_DetonateTheMaximumBombs.java
+++ Java/P_2101_DetonateTheMaximumBombs.java
@@ -1,7 +1,9 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class P_2101_DetonateTheMaximumBombs {
     class Solution {
@@ -30,6 +32,73 @@ public class P_2101_DetonateTheMaximumBombs {
             for (int neighbor : bombGraph.get(bomb)) {
                 if (!seen[neighbor]) {
                     this.dfs(neighbor);
+                }
+            }
+        }
+
+        private void buildGraph(int[][] bombs) {
+            for (int i = 0; i < bombs.length; i++) {
+                bombGraph.put(i, new ArrayList<>());
+            }
+
+            for (int i = 0; i < bombs.length; i++) {
+                for (int j = i + 1; j < bombs.length; j++) {
+                    int[] bombA = bombs[i];
+                    int[] bombB = bombs[j];
+
+                    if (isWithinRadius(bombA, bombB)) {
+                        bombGraph.get(i).add(j);
+                    }
+
+                    if (isWithinRadius(bombB, bombA)) {
+                        bombGraph.get(j).add(i);
+                    }
+                }
+            }
+        }
+
+        private boolean isWithinRadius(int[] bombA, int[] bombB) {
+            long x = bombB[0] - bombA[0];
+            long y = bombB[1] - bombA[1];
+            long radius = bombA[2];
+            return x * x + y * y <= radius * radius;
+        }
+    }
+
+    class Solution2 {
+        Map<Integer, List<Integer>> bombGraph = new HashMap<>();
+        boolean[] seen;
+        int count = 0;
+
+        public int maximumDetonation(int[][] bombs) {
+            this.buildGraph(bombs);
+            int maxCount = 0;
+
+            for (int i = 0; i < bombs.length; i++) {
+                this.seen = new boolean[bombs.length];
+                this.count = 0;
+                this.bfs(i);
+                maxCount = Math.max(maxCount, count);
+            }
+
+            return maxCount;
+        }
+
+        private void bfs(int bomb) {
+            Queue<Integer> queue = new ArrayDeque<>();
+            queue.offer(bomb);
+            this.seen[bomb] = true;
+            this.count++;
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+
+                for (int neighbor : bombGraph.get(node)) {
+                    if (!seen[neighbor]) {
+                        queue.offer(neighbor);
+                        this.seen[neighbor] = true;
+                        this.count++;
+                    }
                 }
             }
         }
