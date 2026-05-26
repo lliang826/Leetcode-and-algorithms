fa0cfe0b2f51c07a98215e7be83bd639c9baa8d5
lliang826 <xlawrence.liangx@gmail.com>
2026-05-25 22:51:20 -0700

    Add bidirectional BFS approach for 127 Java

--- Java/P_127_WordLadder.java
+++ Java/P_127_WordLadder.java
@@ -48,4 +48,55 @@ public class P_127_WordLadder {
             return 0;
         }
     }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+        Set<String> seen = new HashSet<>();
+        Set<String> words = new HashSet<>();
+        for (String s : wordList) {
+            words.add(s);
+        }
+        int count = 0;
+
+        if (!words.contains(endWord)) {
+            return 0;
+        }
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        seen.add(beginWord);
+        seen.add(endWord);
+        count++;
+
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (endSet.size() < beginSet.size()) {
+                Set<String> tempSet = beginSet;
+                beginSet = endSet;
+                endSet = tempSet;
+            }
+
+            Set<String> nextSet = new HashSet<>();
+            for (String s : beginSet) {
+                for (int i = 0; i < s.length(); i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        String newString = s.substring(0, i) + c + s.substring(i + 1, s.length());
+                        if (endSet.contains(newString)) {
+                            return count + 1;
+                        }
+
+                        if (words.contains(newString) && !seen.contains(newString)) {
+                            nextSet.add(newString);
+                            seen.add(newString);
+                        }
+                    }
+                }
+            }
+
+            beginSet = nextSet;
+            count++;
+        }
+
+        return 0;
+    }
 }
