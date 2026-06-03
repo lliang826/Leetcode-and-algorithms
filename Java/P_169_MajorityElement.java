77e3f4463703d1a03fa6dd42c70b1ca5c70b16de
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-06-02 23:57:43 -0700

    Add analysis for 169 Java

--- Java/P_169_MajorityElement.java
+++ Java/P_169_MajorityElement.java
@@ -2,6 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class P_169_MajorityElement {
+    /*
+    Hashmap approach.
+    First, we iterate through the input array and store a count of each element into a hashmap. Then, we iterate
+    on the hashmap; the key with a count greater than the floor of n/2 is the majority element.
+    The question's constraints guarantee that a majority element exists in the input array.
+    
+    Time: O(n), we iterate through the input array and through the hashmap
+    Space: O(n), we build a hashmap using the array's elements
+    */
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
