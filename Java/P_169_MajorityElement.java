262a051eea4c60036aa077b917b8bc3cdc855ec1
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-06-02 23:31:59 -0700

    Add Java solution for 169. Majority Element

---
+++ Java/P_169_MajorityElement.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class P_169_MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+
+        for (int key : map.keySet()) {
+            if (map.get(key) > nums.length / 2) {
+                return key;
+            }
+        }
+
+        return 0;
+    }
+}
