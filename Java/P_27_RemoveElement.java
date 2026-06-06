0b35ce04b9a53a18b11ecf5cfa3a5a25d20cc56b
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-06-06 00:17:23 -0700

    Add alternate solutions for 27 Java

--- Java/P_27_RemoveElement.java
+++ Java/P_27_RemoveElement.java
@@ -16,4 +16,33 @@ public class P_27_RemoveElement {
 
         return count;
     }
+
+    public int v2(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return left;
+    }
+
+    public int v3(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
 }
