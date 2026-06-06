221194d0504f7d86174d586a9c0bedc619e47c31
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-06-05 23:32:38 -0700

    Add initial Java solution for 27. Remove Element

---
+++ Java/P_27_RemoveElement.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class P_27_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                nums[i] = Integer.MAX_VALUE;
+            } else {
+                count++;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        return count;
+    }
+}
