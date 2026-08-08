e94b8812511903b821b71231836079f333e71ffe
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-08-07 22:42:31 -0700

    Add heap approach for 1046 Java

--- Java/P_1046_LastStoneWeight.java
+++ Java/P_1046_LastStoneWeight.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class P_1046_LastStoneWeight {
     public int lastStoneWeight(int[] stones) {
@@ -20,5 +22,23 @@ public class P_1046_LastStoneWeight {
         }
 
         return list.size() == 0 ? 0 : list.get(0);
+    }
+
+    public int v2(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int s : stones) {
+            heap.offer(s);
+        }
+
+        while (heap.size() > 1) {
+            int x = heap.poll();
+            int y = heap.poll();
+            int diff = x - y;
+            if (diff > 0) {
+                heap.offer(diff);
+            }
+        }
+
+        return heap.size() == 0 ? 0 : heap.poll();
     }
 }
