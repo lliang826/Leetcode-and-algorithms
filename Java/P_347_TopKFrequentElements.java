1450f52120bb216007e09f386f40ec351edeed0a
Lawrence Liang <lawrence.liang@hotmail.ca>
2026-08-10 21:46:47 -0700

    Add bucket sort approach for 347 Java

--- Java/P_347_TopKFrequentElements.java
+++ Java/P_347_TopKFrequentElements.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
 public class P_347_TopKFrequentElements {
+    /*
+    Hashmap with heap approach.
+    */
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int n : nums) {
@@ -21,6 +26,38 @@ public class P_347_TopKFrequentElements {
         for (int i = 0; i < k; i++) {
             res[i] = heap.poll();
         }
+        return res;
+    }
+
+    /*
+    Hashmap with bucket sort approach.
+    */
+    public int[] v2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        List<List<Integer>> buckets = new ArrayList<>();
+        for (int i = 0; i < nums.length + 1; i++) {
+            buckets.add(new ArrayList<>());
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            buckets.get(entry.getValue()).add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int counter = 0;
+        for (int i = nums.length; i > 0 && counter < k; i--) {
+            for (int b : buckets.get(i)) {
+                res[counter] = b;
+                counter++;
+                if (counter == k) {
+                    break;
+                }
+            }
+        }
+
         return res;
     }
 }
