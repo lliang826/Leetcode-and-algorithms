06e95df0a85ac0951b48e788d7dea68fbb32c9cb
lliang826 <xlawrence.liangx@gmail.com>
2026-09-20 18:29:15 -0700

    Add alternate solution for 1338 Java

--- Java/P_1338_ReduceArraySizeToTheHalf.java
+++ Java/P_1338_ReduceArraySizeToTheHalf.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -23,6 +26,28 @@ public class P_1338_ReduceArraySizeToTheHalf {
             Map.Entry<Integer, Integer> entry = heap.poll();
             size += entry.getValue();
             count++;
+        }
+
+        return count;
+    }
+
+    public int v2(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer> values = new ArrayList<>(map.values());
+        Collections.sort(values, (a, b) -> b - a);
+
+        int size = 0;
+        int count = 0;
+        for (int i : values) {
+            size += i;
+            count++;
+            if (size >= arr.length / 2) {
+                return count;
+            }
         }
 
         return count;
