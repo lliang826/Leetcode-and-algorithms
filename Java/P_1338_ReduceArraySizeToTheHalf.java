84356e1747d613ec866ebd9535b4c237b9486cba
lliang826 <xlawrence.liangx@gmail.com>
2026-09-20 18:21:25 -0700

    Add Java solution for 1338. Reduce Array Size to The Half

---
+++ Java/P_1338_ReduceArraySizeToTheHalf.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class P_1338_ReduceArraySizeToTheHalf {
+    public int minSetSize(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> {
+            return b.getValue() - a.getValue();
+        });
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            heap.offer(entry);
+        }
+
+        int size = 0;
+        int count = 0;
+        while (size < arr.length / 2) {
+            Map.Entry<Integer, Integer> entry = heap.poll();
+            size += entry.getValue();
+            count++;
+        }
+
+        return count;
+    }
+}
